70cf9064b57958ad2ced9daa31857df014b400de
Piotr Jasiukajtis <estseg@gmail.com>
2012-03-09 16:32:57 +0100

    rm_snoop.d: add systemcall::unlink*:

--- dtrace/rm_snoop.d
+++ dtrace/rm_snoop.d
@@ -11,7 +11,7 @@
 
 BEGIN
 {
-    printf("%10-s %20-s %15-s %15-s %80-s %s \n", "[probemod]", "[probefunc]", "[probename]", "[execname]", "[path]", "[exit code]");
+    printf("%20-s %10-s %20-s %15-s %15-s %80-s %s \n", "[time]", "[probemod]", "[probefunc]", "[probename]", "[execname]", "[path]", "[exit code]");
 }
 
 END
@@ -19,18 +19,50 @@ END
     printf("Finished.\n");
 }
 
-fsinfo:::lookup
+syscall::unlink:entry
 {
     self->me = 1;
-    self->path = args[0]->fi_pathname;
+    self->file = copyinstr(arg0);
+    self->unlinkat = 0;
+    printf("unlink: %s\n", self->file);
 }
+
+syscall::unlinkat:entry
+{
+    self->me = 1;
+    self->file = copyinstr(arg1);
+    self->unlinkat = 1;
+    printf("unlinkat: %s\n", self->file);
+}
+
 
 fsinfo:::remove,rmdir 
-/self->me == 1/ 
+/self->me == 1 && self->unlinkat == 0/ 
 {
-    printf("%10-s %20-s %15-s %15-s %80-s %d \n", probemod, probefunc, probename, execname, self->path, args[1]);
-    self->me = 0;
-    self->path = 0;
+    printf("%Y %10-s %20-s %15-s %15-s %80-s %d \n", walltimestamp, probemod, probefunc, probename, execname, self->file, args[1]);
 }
 
+fsinfo:::remove
+/self->me == 1 && self->unlinkat == 1/ 
+{
+    self->dir = args[0]->fi_pathname;
+    printf("%Y %10-s %20-s %15-s %15-s dir:%s path:%80-s %d \n", walltimestamp, probemod, probefunc, probename, execname, self->dir, self->file, args[1]);
+}
+
+
+fsinfo:::rmdir 
+/self->me == 1 && self->unlinkat == 1/ 
+{
+    self->dir = args[0]->fi_pathname;
+    printf("%Y %10-s %20-s %15-s %15-s %s/%80-s %d \n", walltimestamp, probemod, probefunc, probename, execname, self->dir, self->file, args[1]);
+}
+
+
+syscall::unlink*:return
+/self->me == 1/
+{
+    self->me = 0;
+    self->file = 0;
+    self->unlinkat = 0;
+}
 
