f88fda33aef75cc63861da891c2074b6c34071ba
Piotr Jasiukajtis <estibi@me.com>
2015-08-15 18:31:31 +0200

    compare referenced size in make_zfs_snapshots.sh

--- make_zfs_snapshots.sh
+++ make_zfs_snapshots.sh
@@ -4,30 +4,39 @@ PATH=/usr/bin:/bin:/usr/sbin:/sbin:/opt/local/sbin:/opt/local/bin:/usr/gnu/bin
 
 NOW=$(date '+%y%m%d_%H:%M.%S')
 
-for FS in $(zfs list -H -o name | grep -v rpool/dump | grep -v rpool/swap)
-do
-        echo "Current filesystem: ${FS}"
-        unset snap_array
-        for snapshot in $(zfs list -r -t snapshot -H -o name ${FS})
-        do
-                snap_array=("${snap_array[@]}" "${snapshot}")
-        done
+RPOOL_NAME=$(zfs list -H -o name / | awk -F"/" '{print $1}')
 
-        if [ "${#snap_array[@]}" -gt 1 ]; then
-                #echo "${snap_array[@]}"
-                # filesystem has a snapshots
-                snap_amount=${#snap_array[@]}
-                offset=$(expr ${snap_amount} - 1)
-                latest_snap_name=${snap_array[${offset}]}
-                used_size=$(zfs get -Hp -o value used ${latest_snap_name})
-                if [ ${used_size} -eq 0 ]; then
-                        # no data were changed since the latest snapshot
-                        continue
-                fi
+echo "RPOOL_NAME: ${RPOOL_NAME}"
+
+for FS in $(zfs list -H -o name | grep -v ${RPOOL_NAME}/dump | grep -v ${RPOOL_NAME}/swap)
+do
+    echo "Current filesystem: ${FS}"
+    unset snap_array
+    for snapshot in $(zfs list -r -t snapshot -H -o name ${FS})
+    do
+        snap_array=("${snap_array[@]}" "${snapshot}")
+    done
+
+    if [ "${#snap_array[@]}" -gt 1 ]; then
+            #echo "${snap_array[@]}"
+            # filesystem has a snapshots
+            snap_amount=${#snap_array[@]}
+            offset=$(expr ${snap_amount} - 1)
+            latest_snap_name=${snap_array[${offset}]}
+            used_size=$(zfs get -Hp -o value used ${latest_snap_name})
+        if [ ${used_size} -eq 0 ]; then
+            # compare referenced size
+            referenced_snap=$(zfs get -Hp -o value referenced ${latest_snap_name})
+            referenced_fs=$(zfs get -Hp -o value referenced ${FS})
+            if [ ${referenced_snap} -eq ${referenced_fs} ]; then
+                # no data were changed since the latest snapshot
+                continue
+            fi
         fi
-        echo
-        echo "Creating new snapshot: ${FS}@backup_${NOW}"
-        zfs snapshot ${FS}@backup_${NOW}
+    fi
+    echo "Creating new snapshot: ${FS}@backup_${NOW}"
+    zfs snapshot ${FS}@backup_${NOW}
+    echo
 done
 
 # EOF
